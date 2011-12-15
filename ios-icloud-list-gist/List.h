7b1492d39a616472c04120a10869c5fce49f77c6
Brian Hill <brian@lingerhere.org>
2011-12-14 16:16:34 -0800

    Added UIDocument subclass.

---
+++ ios-icloud-list-gist/List.h
@@ -0,0 +1,17 @@
+//
+//  List.h
+//  ios-icloud-list-gist
+//
+//  Created by Brian Hill on 12/14/11.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface List : UIDocument
+
+@property (strong) NSString *text;
+@property (weak) DetailViewController *listDelegate;
+
+@end
---
+++ ios-icloud-list-gist/List.m
@@ -0,0 +1,39 @@
+//
+//  List.m
+//  ios-icloud-list-gist
+//
+//  Created by Brian Hill on 12/14/11.
+//
+
+#import "List.h"
+#import "DetailViewController.h"
+
+@implementation List
+
+@synthesize text = _text;
+@synthesize listDelegate = _listDelegate;
+
+- (BOOL)loadFromContents:(id)contents ofType:(NSString *)typeName error:(NSError **)outError {
+    if ([contents length] > 0) {
+        self.text = [[NSString alloc] initWithData:contents encoding:NSUTF8StringEncoding];
+    } else {
+        self.text = @"";
+    }
+    
+    // [self.listDelegate contentsUpdated:self];
+    return TRUE;
+}
+
+- (id)contentsForType:(NSString *)typeName error:(NSError **)outError {
+    return [self.text dataUsingEncoding:NSUTF8StringEncoding];
+}
+
+- (void)disableEditing {
+    [self.listDelegate disableEditing:self];
+}
+
+- (void)enableEditing {
+    [self.listDelegate enableEditing:self];
+}
+
+@end
